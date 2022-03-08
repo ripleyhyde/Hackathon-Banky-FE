ed8e983eca9ae53251c5298f9bc6fc8d34ac73e4
zeesairafi <alsairafizainab19@gmail.com>
2022-03-08 14:58:26 +0300

    done

--- banky/src/Components/DashBoard.js
+++ banky/src/Components/DashBoard.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 const DashBoard = () => {
-  return <div>DashBoard</div>;
+  //   // State to store count value
+  //   const [count, setCount] = useState(7);
+
+  //   // Function to increment count by 1
+  //   const incrementCount = () => {
+  //     // Update state with incremented value
+  //     setCount(count + 1);
+  //   };
+  return (
+    <div className="app">
+      <p>Quick Pay</p>
+      <p>7</p>
+      <div>
+        <p>Transfers: 5</p>
+        <p> Total Incoming: KD 6,000.00</p>
+        <p>Transfers: 3</p>
+        <p>Total Outgoing: KD 3,000.00</p>
+      </div>
+      <div>
+        <p>Number of Beneficiaries: 5</p>
+        <button>View Beneficiaries</button>
+      </div>
+      <div>
+        <p>Total Accounts Balance: KD 10,500.00</p>
+      </div>
+    </div>
+  );
 };
 
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<DashBoard />, rootElement);
 export default DashBoard;
