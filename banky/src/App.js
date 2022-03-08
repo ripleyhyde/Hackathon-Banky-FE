9b52a231e1cdd96a324f7b246f90e04c85f9196d
FAlSaleh94 <94alsaleh@gmail.com>
2022-03-08 09:29:07 +0300

    CSS

--- banky/src/App.js
+++ banky/src/App.js
@@ -1,11 +1,11 @@
-import UserForm from "./components/UserForm";
+import UserForm from "./Components/UserForm";
 import "./App.css";
 import Cards from "./Components/Cards";
 
 function App() {
   return (
     <div className="App">
-      <Cards />
+      {/* <Cards /> */}
       <UserForm />
     </div>
   );
--- banky/src/components/UserForm.js
+++ banky/src/components/UserForm.js
@@ -30,87 +30,116 @@ const UserForm = () => {
   };
   return (
     <div>
+      <img
+        src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80"
+        alt="Girl in a jacket"
+      />
       <h3>
         {authstore.user ? (
-          <>
-            <button onClick={signout}>Sign Out</button>
-          </>
+          <div>
+            <button onClick={signout} className="button">
+              Sign Out
+            </button>
+          </div>
         ) : (
-          <button onClick={() => setIsOpen(true)}>Sign Up</button>
+          <button onClick={() => setIsOpen(true)} className="button">
+            Sign Up
+          </button>
         )}
       </h3>
-      <Modal centered show={isOpen} onHide={() => setIsOpen(false)}>
-        <Modal.Header closeButton>
+      <Modal
+        centered
+        show={isOpen}
+        onHide={() => setIsOpen(false)}
+        className="form"
+      >
+        <Modal.Header>
           <Modal.Title>
             <BsPersonCircle /> User
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form onSubmit={handleSubmit}>
-            <Form.Label>First Name</Form.Label>
+            {/* <Form.Label className="text"></Form.Label> */}
             <Form.Control
+              className="box"
               name="firstName"
               value={user.firstName}
               type="text"
               onChange={handleChange}
+              placeholder="First Name"
             />
             <div>
-              <Form.Label>Last Name</Form.Label>
+              {/* <Form.Label className="text">Last Name</Form.Label> */}
               <Form.Control
+                className="box"
                 name="lastName"
                 value={user.lastName}
                 type="text"
                 onChange={handleChange}
+                placeholder="Last Name"
               />
             </div>
             <div>
-              <Form.Label>User Name</Form.Label>
+              {/* <Form.Label className="text">User Name</Form.Label> */}
               <Form.Control
+                className="box"
                 name="username"
                 value={user.username}
                 type="text"
                 onChange={handleChange}
+                placeholder="User Name"
               />
             </div>
             <div>
-              <Form.Label>Phone Number</Form.Label>
+              {/* <Form.Label className="text">Phone Number</Form.Label> */}
               <Form.Control
+                className="box"
                 name="phoneNumber"
                 value={user.phoneNumber}
                 type="Number"
                 onChange={handleChange}
+                placeholder="Phone Number"
               />
             </div>
             <div>
-              <Form.Label>Email</Form.Label>
+              {/* <Form.Label className="text">Email</Form.Label> */}
               <Form.Control
+                className="box"
                 name="email"
                 type="email"
                 value={user.email}
                 onChange={handleChange}
+                placeholder="Email"
               />
             </div>
             <div>
-              <Form.Label>Password</Form.Label>
+              {/* <Form.Label className="text">Password</Form.Label> */}
               <Form.Control
+                className="box"
                 name="password"
                 type="password"
                 value={user.password}
                 onChange={handleChange}
+                placeholder="Password"
               />
             </div>
+            <Modal.Footer>
+              <div className="btn">
+                <button value="login" onClick={handleSubmit} className="button">
+                  Sign In
+                </button>
+                <button
+                  value="signUp"
+                  onClick={handleSubmit}
+                  className="button"
+                >
+                  Sign Up
+                </button>
+              </div>
+            </Modal.Footer>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <div>
-            <button value="login" onClick={handleSubmit}>
-              Sign In
-            </button>
-            <button value="signUp" onClick={handleSubmit}>
-              Sign Up
-            </button>
-          </div>
-        </Modal.Footer>
       </Modal>
     </div>
   );
